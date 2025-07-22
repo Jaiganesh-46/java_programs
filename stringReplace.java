f48ce6dcda13533dc72f394f35dcab22f425ae13
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-07-22 09:44:20 +0530

    Add or update stringReplace.java

--- stringReplace.java
+++ stringReplace.java
@@ -12,9 +12,34 @@ public class stringReplace {
                 case '1' -> sb.append('2');
                 default -> sb.append(ch);
             }
-
         }
         System.out.println(sb.toString());
-
     }
 }
+
+/* Alternative implementation without using StringBuilder:
+
+import java.util.*;
+public class stringReplace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] result = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'A') {
+                result[i] = 'B';
+            } else if (ch == 'a') {
+                result[i] = '@';
+            } else if (ch == '1') {
+                result[i] = '2';
+            } else {
+                result[i] = ch;
+            }
+        }
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+        }
+        System.out.println();
+    }
+}*/
