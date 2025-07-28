771d8f001a57f9932c145794752f6f35e5012cbb
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-07-28 12:30:11 +0530

    Add zigzag pattern program

---
+++ patterns/zigzag.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class zigzag {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 4*n-1; j++) {
+                if ((i+j)%4 == 0 || (i == 2 && j%4 == 0)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
