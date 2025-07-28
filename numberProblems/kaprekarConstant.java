4c8f674b65488c78c7580c68864926e861cbbe19
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-07-28 16:35:21 +0530

    Kaprekar constant

--- numberProblems/kaprekarConstant.java
+++ numberProblems/kaprekarConstant.java
@@ -6,25 +6,18 @@ public class kaprekarConstant {
         int count = 0;
         
         while (n != 6174) {
-            // Convert to string and pad with zeros if needed
             String numStr = String.format("%04d", n);
-            
-            // Extract digits
             int[] digits = new int[4];
             for (int i = 0; i < 4; i++) {
                 digits[i] = numStr.charAt(i) - '0';
             }
             
-            // Sort digits in ascending order
             Arrays.sort(digits);
             
-            // Form smallest number
             int min = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
             
-            // Form largest number
             int max = digits[3] * 1000 + digits[2] * 100 + digits[1] * 10 + digits[0];
             
-            // Calculate difference
             n = max - min;
             count++;
         }
