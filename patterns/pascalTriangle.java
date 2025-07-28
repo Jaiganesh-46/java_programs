b1aa8db94d0fe75db52a4bd7078889795504dcd1
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-07-28 12:21:23 +0530

    Add Pascal Triangle pattern

---
+++ patterns/pascalTriangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class pascalTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int s = 0; s < n-i-1; s++) {
+                System.out.print(" ");
+            }
+            int c = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(c + " ");
+                c = c * (i-j) / (j+1);
+            }
+            System.out.println();
+        }
+    }
+}
