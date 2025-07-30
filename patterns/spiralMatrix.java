8648e9d735cbceefa234b09b076aae0b5828be09
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-07-30 10:37:44 +0530

    Update spiral matrix pattern

--- patterns/spiralMatrix.java
+++ patterns/spiralMatrix.java
@@ -1,38 +1,22 @@
-import java.util.*;
 public class spiralMatrix {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int n = sc.nextInt();
-        int[][] matrix = new int[n][n];
-        int value = 1;
+        int n = 5, val = 1;
+        int[][] spiral = new int[n][n];
         int top = 0, bottom = n - 1, left = 0, right = n - 1;
-        while (top <= bottom && left <= right) {
-            for (int i = left; i <= right; i++) {
-                matrix[top][i] = value++;
-            }
+
+        while (val <= n * n) {
+            for (int i = left; i <= right; i++) spiral[top][i] = val++;
             top++;
-            for (int i = top; i <= bottom; i++) {
-                matrix[i][right] = value++;
-            }
+            for (int i = top; i <= bottom; i++) spiral[i][right] = val++;
             right--;
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    matrix[bottom][i] = value++;
-                }
-                bottom--;
-            }
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    matrix[i][left] = value++;
-                }
-                left++;
-            }
+            for (int i = right; i >= left; i--) spiral[bottom][i] = val++;
+            bottom--;
+            for (int i = bottom; i >= top; i--) spiral[i][left] = val++;
+            left++;
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
+
+        for (int[] row : spiral) {
+            for (int x : row) System.out.print(x + " ");
             System.out.println();
         }
     }
