783537cde1fc3e720710803d47a1a71792f02e2d
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-07-30 11:34:49 +0530

    Add Java programs and number problems

---
+++ numberProblems/armstrongNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class armstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int temp = n;
+        while (temp > 0){
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+        if (sum == n){
+            System.out.println("Yes, " + n + " is an Armstrong number.");
+        } else {
+            System.out.println("No, " + n + " is not an Armstrong number.");
+        }
+    }
+}
