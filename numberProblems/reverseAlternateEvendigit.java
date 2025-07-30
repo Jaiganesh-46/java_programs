20c46b24fdc4f5c7a891e47c00423ee5f7985f53
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-07-30 10:35:05 +0530

    Add reverse alternate even digits program

---
+++ numberProblems/reverseAlternateEvendigit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class reverseAlternateEvendigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        String numStr = String.valueOf(n);
+        int len = numStr.length();
+        
+        for (int i = len - 1; i >= 0; i -= 2) {
+            System.out.print(numStr.charAt(i) + " ");
+        }
+    }
+}
