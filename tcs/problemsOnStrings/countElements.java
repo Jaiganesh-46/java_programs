cf6a657935698b35a360efd4ba0f42a536e2c06b
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-08-01 10:39:22 +0530

    Add countElements

---
+++ tcs/problemsOnStrings/countElements.java
@@ -0,0 +1,21 @@
+package problemsOnStrings;
+
+import java.util.*;
+public class countElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        int v = 0, c = 0, s = 0;
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            switch (ch) {
+                case 'a', 'e', 'i', 'o', 'u' -> v++;
+                case ' ' -> s++;
+                default -> c++;
+            }
+        }
+        System.out.println("Vowels: " + v);
+        System.out.println("Consonants: " + c);
+        System.out.println("Spaces: " + s);
+    }
+}
