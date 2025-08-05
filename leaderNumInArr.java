3179a2849275e6240019fb3e676070b7ea0fed7d
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-08-05 09:53:49 +0530

    Add leaderNumInArr.java

---
+++ leaderNumInArr.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class leaderNumInArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int leader = arr[arr.length - 1];
+        System.out.print(leader + " ");
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > leader) {
+                leader = arr[i];
+                System.out.print(leader + " ");
+            }
+        }
+    }
+}
