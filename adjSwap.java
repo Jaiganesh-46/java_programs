aaaa578a05c47f607ce36ebd42b27e75acee1cf3
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-08-05 12:43:53 +0530

    Update java program

---
+++ adjSwap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class adjSwap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < arr.length - 1; i+=2) {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
