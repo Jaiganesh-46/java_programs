d36d014ae4652b871de78bc65f301dde05c71258
Jaiganesh-46 <jaiganesh4341@gmail.com>
2025-10-07 11:36:13 +0530

    Add OOP CarDemo class and fix directory naming issue
    - Added CarDemo.java demonstrating basic OOP concepts
    - Renamed directory from 'class&objects' to 'classAndObjects' to fix build path issues
    - Includes Car class with properties (brand, model, price) and methods
    - Demonstrates object creation and method calling

---
+++ oops/classAndObjects/CarDemo.java
@@ -0,0 +1,27 @@
+class CarDemo {
+    String brand;
+    String model;
+    double price;
+
+    void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: ₹" + price);
+        System.out.println("----------------------");
+    }
+
+    public static void main(String[] args) {
+        CarDemo car1 = new CarDemo();
+        car1.brand = "Tesla";
+        car1.model = "Model 3";
+        car1.price = 3500000;
+
+        CarDemo car2 = new CarDemo();
+        car2.brand = "Tata";
+        car2.model = "Nexon EV";
+        car2.price = 1800000;
+
+        car1.displayInfo();
+        car2.displayInfo();
+    }
+}
